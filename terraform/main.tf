ca467789c5903aa6c78cf35868d8860177b70a10
lorenzo <lorenz-gcp@de-zoomcamp-vminsta.europe-west6-a.c.diesel-air-394514.internal>
2024-04-11 13:15:40 +0000

    added iac commands for bucket and bigquery dataset

---
+++ terraform/main.tf
@@ -0,0 +1,42 @@
+terraform {
+  required_providers {
+    google = {
+      source  = "hashicorp/google"
+      version = "5.6.0"
+    }
+  }
+}
+
+provider "google" {
+  credentials = file(var.credentials)
+  project     = var.project
+  region      = var.region
+}
+
+
+resource "google_storage_bucket" "demo-bucket" {
+  name          = var.gcs_bucket_name
+  location      = var.region
+  force_destroy = true
+
+
+  lifecycle_rule {
+    condition {
+      age = 1
+    }
+    action {
+      type = "AbortIncompleteMultipartUpload"
+    }
+  }
+}
+
+
+
+resource "google_bigquery_dataset" "demo_dataset" {
+  dataset_id = var.bq_dataset_name
+  project    = var.project
+  location   = var.region
+}
+
+
+# TODO ADD DATAPROC
---
+++ terraform/variables.tf
@@ -0,0 +1,36 @@
+variable "credentials" {
+  description = "My Credentials"
+  default     = "/home/lorenz/batch-processing-fifa-dataset-on-gcp/terraform/svcaccount.json"
+  #ex: if you have a directory where this file is called keys with your service account json file
+  #saved there as my-creds.json you could use default = "./keys/my-creds.json"
+}
+
+
+variable "project" {
+  description = "Project"
+  default     = "diesel-air-394514"
+}
+
+variable "region" {
+  description = "Region"
+  #Update the below to your desired region
+  default     = "europe-west6-a"
+}
+
+variable "bq_dataset_name" {
+  description = "My BigQuery Dataset Name"
+  #Update the below to what you want your dataset to be called
+  default     = "diesel-air-394514"
+}
+
+variable "gcs_bucket_name" {
+  description = "My Storage Bucket Name"
+  #Update the below to a unique bucket name
+  default     = "mage-zoomcamp-bucket-lm"
+}
+
+variable "gcs_storage_class" {
+  description = "Bucket Storage Class"
+  default     = "STANDARD"
+}
+
