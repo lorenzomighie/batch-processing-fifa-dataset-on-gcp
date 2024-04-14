14c44d9f2e2e00803b861d687b285d196dd40025
Lorenzo Michieletto <l.michieletto@nexidigital.eu>
2024-04-14 14:07:41 +0200

    added dataproc in terraform

--- terraform/main.tf
+++ terraform/main.tf
@@ -39,4 +39,30 @@ resource "google_bigquery_dataset" "demo_dataset" {
 }
 
 
-# TODO ADD DATAPROC
+resource "google_dataproc_cluster" "dataproc-cluster" {
+  name     = var.spark_cluster_name
+  project    = var.project_name
+  region   = var.region
+
+  cluster_config {
+    staging_bucket = var.gcs_bucket_name
+
+    master_config {
+      num_instances = 1
+      machine_type  = "n2-standard-2"
+      disk_config {
+        boot_disk_type    = "pd-ssd"
+        boot_disk_size_gb = 50
+      }
+    }
+
+    # Override or set some custom properties
+    software_config {
+      image_version = "2.0-debian10"
+      override_properties = {
+        "dataproc:dataproc.allow.zero.workers" = "true"
+      }
+    }
+
+  }
+}
