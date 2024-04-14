349f727b8893b92c20f5861c428b06cf256b2d51
Lorenzo <45019206+lorenzomighie@users.noreply.github.com>
2024-04-14 15:06:49 +0200

    Update queries_partition.sql

--- bigquery/queries_partition.sql
+++ bigquery/queries_partition.sql
@@ -45,3 +45,69 @@ FROM diesel-air-394514.fifa_schema.fifa_transformed_dataset_partitioned_clustere
 WHERE version = 17 and gender = 'male' and club_name = 'Chelsea';
 -- 2.69 MB estimated and effective
 
+
+-- best player by year
+SELECT version, short_name, gender, overall
+FROM diesel-air-394514.fifa_schema.fifa_transformed_dataset_partitioned_clustered as f1
+WHERE overall = 
+( SELECT MAX(overall)
+  FROM diesel-air-394514.fifa_schema.fifa_transformed_dataset_partitioned_clustered as f2
+  WHERE f1.version = f2.version
+)
+ORDER BY version;
+
+
+-- Best team for a selected version, wrt to the best 11 players
+SELECT club_name, SUM(overall) total
+FROM (
+    SELECT version, gender, club_name, overall, ROW_NUMBER() OVER(PARTITION BY club_name ORDER BY overall DESC) rn
+    FROM diesel-air-394514.fifa_schema.fifa_transformed_dataset_partitioned
+    WHERE version = 22 and gender = 'male' and club_name <> 'unknown'
+) x 
+WHERE rn <= 11
+GROUP BY club_name
+ORDER BY total DESC
+LIMIT 5;
+
+-- show club names for female gender
+SELECT club_name
+FROM diesel-air-394514.fifa_schema.fifa_transformed_dataset_partitioned_clustered
+WHERE gender = 'female'
+GROUP BY club_name;
+-- result --> all have 'unknown' value
+
+
+-- Sum of the wage of the 100 most payed players year by year
+SELECT version, SUM(wage_eur) total_wage
+FROM (
+    SELECT version, gender, club_name, wage_eur, ROW_NUMBER() OVER(PARTITION BY version ORDER BY wage_eur DESC) rn
+    FROM diesel-air-394514.fifa_schema.fifa_transformed_dataset_partitioned_clustered
+) x 
+WHERE rn <= 100
+GROUP BY version
+ORDER BY version ASC;
+
+
+-- Best potential team for a certain version and gender wrt to all (30) players of a team
+SELECT club_name, SUM(potential) total
+FROM (
+    SELECT version, gender, club_name, potential, ROW_NUMBER() OVER(PARTITION BY club_name ORDER BY potential DESC) rn
+    FROM diesel-air-394514.fifa_schema.fifa_transformed_dataset_partitioned
+    WHERE version = 22 and gender = 'male' and club_name <> 'unknown'
+) x 
+WHERE rn <= 30
+GROUP BY club_name
+ORDER BY total DESC
+LIMIT 5;
+
+-- Best nation team for selected year and gender
+SELECT nationality_name, SUM(overall) total
+FROM (
+    SELECT version, gender, nationality_name, overall, ROW_NUMBER() OVER(PARTITION BY nationality_name ORDER BY overall DESC) rn
+    FROM diesel-air-394514.fifa_schema.fifa_transformed_dataset_partitioned
+    WHERE version = 22 and gender = 'female' and nationality_name <> 'unknown'
+) x 
+WHERE rn <= 11
+GROUP BY nationality_name
+ORDER BY total DESC
+LIMIT 5;
